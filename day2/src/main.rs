b9b6eca4a6e6ce12338e46992288ef66c219c8ab
Ben Richey <b.richeypg@gmail.com>
2024-12-03 20:41:41 -0500

    complete puzzle 2 of day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs;
 
-const PROBLEM_THRESHOLD: i32 = 1; // For part 1, set to 0. For part two, set to 1.
+const PROBLEM_THRESHOLD: i32 = 0; // For part 1, set to 0. For part two, set to 1.
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -52,11 +52,43 @@ fn main() {
         }
         safe_indices.push(safe);
     }
-    for iter in data.iter().zip(safe_indices.iter()) {
-        let (line_ref, safe_ref) = iter;
-        if *safe_ref == false {
-            println!("{:?} is safe: {}", *line_ref, *safe_ref);
+    println!("Number of safe reports: {}", safe_indices.iter().filter(|&x| *x == true).count());
+    let mut safe_indices: Vec<bool> = Vec::new();
+    safe_indices.reserve(size);
+    for line in &data {
+        let mut global_safe = false;
+        for leave_out_idx in 0..(*line).len() {
+            let mut lt = false;
+            let mut gt = false;
+            let mut line_clone = line.clone();
+            line_clone.remove(leave_out_idx);
+            let mut iter = line_clone.iter();
+            let mut curr = iter.next();
+            let mut next = iter.next();
+            let mut safe = true;
+            while next.is_none() == false {
+                let diff = curr.unwrap() - next.unwrap();
+                curr = next;
+                next = iter.next();
+                if diff < 0 {
+                    lt = true;
+                } else if diff > 0 {
+                    gt = true;
+                }
+                if diff.abs() == 0 || diff.abs() > 3 {
+                    safe = false;
+                    next = None;
+                } else if gt == true && lt == true {
+                    safe = false;
+                    next = None;
+                }
+            }
+            if safe == true {
+                global_safe = true;
+                break;
+            }
         }
+        safe_indices.push(global_safe);
     }
     println!("Number of safe reports: {}", safe_indices.iter().filter(|&x| *x == true).count());
 }
